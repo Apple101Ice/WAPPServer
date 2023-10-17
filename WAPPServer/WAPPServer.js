32d0843b9af244fb8ba6ec9b46cc6304a4efa923
Apple101Ice <mercenaryxx@gmail.com>
2023-10-17 16:50:07 +0530

    Update WAPPServer.js

--- WAPPServer/WAPPServer.js
+++ WAPPServer/WAPPServer.js
@@ -51,7 +51,7 @@ app.use(passport.initialize());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Replace '*' with specific origins if needed
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Include 'Authorization' header
     next();
 });
 
